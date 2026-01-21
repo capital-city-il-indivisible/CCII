689af0ce22c423dcd5ff3ca4eeb7247d7eec2841
Valorieb <valoriebroderick@gmail.com>
2026-01-21 16:39:10 -0600

    made js to loop through events

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("scripts");
 
+  eleventyConfig.addCollection("events", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("content/webmaster-events/*.md");
+  });
+
   return {
     dir: {
       input: "content",
