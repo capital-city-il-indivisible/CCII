03ef6a38c784a519905e98cb5bb59ea395af8b7e
Valorieb <valoriebroderick@gmail.com>
2026-01-21 15:54:36 -0600

    created passthrough for scripts moved html files inside content folder.

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function (eleventyConfig) {
   
   eleventyConfig.addPassthroughCopy("styles");
   eleventyConfig.addPassthroughCopy("images");
-  eleventyConfig.addPassthroughCopy("index.html");
+  eleventyConfig.addPassthroughCopy("scripts");
 
   return {
     dir: {
