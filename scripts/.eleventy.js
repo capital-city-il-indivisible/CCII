ec5a0fd9c26c9f8aa22a512b7fbef48ea0cec23f
Valorieb <valoriebroderick@gmail.com>
2026-01-21 14:57:44 -0600

    updated return statement with directory paths

--- scripts/.eleventy.js
+++ scripts/.eleventy.js
@@ -7,8 +7,8 @@ module.exports = function (eleventyConfig) {
 
   return {
     dir: {
-      input: "content",
-      output: "_site",
+      input: "../content",
+      output: "../_site",
       includes: "../_includes",
     },
   };
