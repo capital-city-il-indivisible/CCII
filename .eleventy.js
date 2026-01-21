d0b83c865169a51ee645f39b82e81e4f1c6554ee
Valorieb <valoriebroderick@gmail.com>
2026-01-21 16:49:43 -0600

    added js for date and time simplicity

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,17 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
-  
   eleventyConfig.addPassthroughCopy("styles");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("scripts");
 
   eleventyConfig.addCollection("events", function (collectionApi) {
     return collectionApi.getFilteredByGlob("content/webmaster-events/*.md");
+  });
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    if (!dateObj) return "";
+    return DateTime.fromJSDate(dateObj).toFormat("MMMM d, yyyy");
   });
 
   return {
