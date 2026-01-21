045c3f9186e83e1d05e2ce1e7e305b3c408330ca
Valorieb <valoriebroderick@gmail.com>
2026-01-21 15:10:39 -0600

    "moved eleventy.js to root"

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function (eleventyConfig) {
+  // Copy entire styles folder
+  eleventyConfig.addPassthroughCopy("styles");
+
+  // Copy images folder
+  eleventyConfig.addPassthroughCopy("images");
+
+  return {
+    dir: {
+      input: "content",
+      output: "_site",
+      includes: "_includes",
+    },
+  };
+};
