8802766bf526c42d96894aafe73a17c0f3d9b88f
Valorieb <valoriebroderick@gmail.com>
2026-02-21 13:44:47 -0600

    Updated styles

--- scripts/nav.js
+++ scripts/nav.js
@@ -11,35 +11,35 @@ navUL.classList.add("menuLinks");
 navbar.appendChild(navUL);
 
 const navLinks = [
-  { text: "Home", href: "/index.html" },
-  {text: "About", href: "/about.html"},
-  { text: "Events", href: "/events.html" },
-  { text: "Join", href: "/join.html" },
-  {text: "Support CCII", href: "/support.html"},
-  {text: "Contact Us", href: "/contact.html"},
-  {text: "Resources", href: "/resources.html"}
+  { text: "Home", href: "/index/" },
+  { text: "About", href: "/about/" },
+  { text: "Events", href: "/events/l" },
+  { text: "Join", href: "/join/" },
+  { text: "Support CCII", href: "/support/" },
+  { text: "Contact Us", href: "/contact/" },
+  { text: "Resources", href: "/resources/" },
 ];
 
 navLinks.forEach((link) => {
-    const path = window.location.pathname;
-    const page = path.split("/").pop();
+  const path = window.location.pathname;
+  const page = path.split("/").pop();
 
-    const navLI = document.createElement("li");
-    navUL.appendChild(navLI);
+  const navLI = document.createElement("li");
+  navUL.appendChild(navLI);
 
-    const linkEl = document.createElement("a");
-    navLI.appendChild(linkEl);
-    linkEl.setAttribute("href", `${link.href}`);
-    linkEl.setAttribute("title", `${link.text}`);
-    linkEl.textContent = `${link.text}`;
-    navLI.appendChild(linkEl);
+  const linkEl = document.createElement("a");
+  navLI.appendChild(linkEl);
+  linkEl.setAttribute("href", `${link.href}`);
+  linkEl.setAttribute("title", `${link.text}`);
+  linkEl.textContent = `${link.text}`;
+  navLI.appendChild(linkEl);
 
-    if (link.href === page){
-        linkEl.classList.add("active");
-    }
+  if (link.href === page) {
+    linkEl.classList.add("active");
+  }
 });
 
 hamButton.addEventListener("click", () => {
-    navUL.classList.toggle("open");
-    hamButton.classList.toggle("open");
+  navUL.classList.toggle("open");
+  hamButton.classList.toggle("open");
 });
